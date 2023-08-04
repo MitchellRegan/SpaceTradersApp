e182b55e05e90d0f91f5a539a711fb948fa2a9db
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2023-08-04 09:46:28 -0500

    - Finished copying over the rest of the system-related API calls from the documentation over to navigation-api-calls.js - Working on copying over the rest of the ship-related API calls to ship-api-calls.js

--- api-calls/navigation-api-calls.js
+++ api-calls/navigation-api-calls.js
@@ -4,7 +4,41 @@
 import sa from './server-address';
 const NavigationAPICalls = {
     /**
-     * API call to get info about a specific system.
+     * Returns a paginated list of all systems in the game.
+     * https://spacetraders.stoplight.io/docs/spacetraders/94269411483d0-list-systems
+     * @param {int} resultsPerPage_ How many systems will be returned per page. Defaults to 20. Range is 1-20.
+     * @param {int} pageNum_ The page number of the list of systems to view. Defaults to 1.
+     */
+    listSystems: async function (resultsPerPage_=20, pageNum_=1) {
+        const localData = require("../user-preferences.json");
+
+        let callData = await fetch(sa.address + 'systems/?page=' + pageNum_.toString() + '&limit=' + resultsPerPage_.toString(), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localData.token
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data
+            })
+            .catch((error) => {
+                return {
+                    error: {
+                        title: "NavigationAPICalls.listSystems Error",
+                        message: error
+                    }
+                }
+            })
+
+        return callData;
+    },
+
+
+    /**
+     * Get the details of a specific system.
+     * https://spacetraders.stoplight.io/docs/spacetraders/67e77e75c65e7-get-system
      * @param {string} systemSymbol_ The name of the system to get info about.
      */
     getSystem: async function (systemSymbol_) {
@@ -34,9 +68,44 @@ const NavigationAPICalls = {
     },
 
 
+    /** 
+     * Returns a paginated list of all of the waypoints for a given system. If a waypoint is uncharted, it will return the [Uncharted] trait instead of its actual traits.
+     * https://spacetraders.stoplight.io/docs/spacetraders/32186cf59e324-list-waypoints-in-system
+     * @param {string} systemSymbol_ The name of the system to get waypoints from.
+     * @param {int} resultsPerPage_ How many waypoints will be returned per page. Defaults to 20. Range is 1-20.
+     * @param {int} pageNum_ The page number of the list of waypoints to view. Defaults to 1.
+     */
+    listWaypointsInSystem: async function (systemSymbol_, resultsPerPage_ = 20, pageNum_ = 1) {
+        const localData = require("../user-preferences.json");
+
+        let callData = await fetch(sa.address + 'systems/' + systemSymbol_ + '/waypoints?page=' + pageNum_.toString() + '&limit=' + resultsPerPage_.toString(), {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localData.token
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                return {
+                    error: {
+                        title: "NavigationAPICalls.listWaypointsInSystem Error",
+                        message: error
+                    }
+                }
+            })
+
+        return callData;
+    },
+
+
     /**
-     * API call to get info about a specific waypoint.
-     * @param {string} waypointSymbol_ The name of the symbol to get info about.
+     * View the details of a waypoint. If the waypoint is uncharted, it will return the [Uncharted] trait instead of its actual traits.
+     * https://spacetraders.stoplight.io/docs/spacetraders/58e66f2fa8c82-get-waypoint
+     * @param {string} waypointSymbol_ The name of the waypoint to get info about.
      * @param {string} systemSymbol_ The name of the system that the waypoint is in.
      */
     getWaypoint: async function (waypointSymbol_, systemSymbol_) {
@@ -67,16 +136,19 @@ const NavigationAPICalls = {
 
 
     /** 
-     * API call to get a list of all systems in the universe.
-     * @param {string} systemSymbol_ The name of the system to get waypoints from.
-     * @returns List of system waypoints, their unique symbol names, their (x,y) coordinates, traits, and the locations within their orbit (i.e. things like moons).
+     * Retrieve imports, exports, and exchange data from a marketplace. Requires a waypoint that has the [Marketplace] trait to use.
+     * Send a ship to the waypoint to access trade good prices and recent transactions.
+     * https://spacetraders.stoplight.io/docs/spacetraders/a4fed7a0221e0-get-market
+     * @param {string} waypointSymbol_ The name of the waypoint with the [Marketplace] trait.
+     * @param {string} systemSymbol_ The name of the system that the market waypoint is in.
      */
-    getSystemsWaypointList: async function (systemSymbol_) {
+    getMarket: async function (waypointSymbol_, systemSymbol_) {
         const localData = require("../user-preferences.json");
 
-        let callData = await fetch(sa.address + 'systems/' + systemSymbol_ + '/waypoints', {
+        let callData = await fetch(sa.address + 'systems/' + systemSymbol_ + '/waypoints/' + waypointSymbol_ + '/market', {
+            method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
+                'Accept': 'application/json',
                 'Authorization': 'Bearer ' + localData.token
             },
         })
@@ -87,14 +159,82 @@ const NavigationAPICalls = {
             .catch((error) => {
                 return {
                     error: {
-                        title: "NavigationAPICalls.getSystemWaypointsList Error",
+                        title: "NavigationAPICalls.getMarket Error",
                         message: error
                     }
                 }
             })
 
         return callData;
-    }
+    },
+
+
+    /** 
+     * Get the shipyard from a waypoint. Requires a waypoint that has the [Shipyard] trait to use.
+     * Send a ship to the waypoint to access data on ships that are currently available for purchase and recent transactions.
+     * https://spacetraders.stoplight.io/docs/spacetraders/460fe70c0e4c2-get-shipyard
+     * @param {string} waypointSymbol_ The name of the waypoint with the [Shipyard] trait.
+     * @param {string} systemSymbol_ The name of the system that the shipyard waypoint is in.
+     */
+    getShipyard: async function (waypointSymbol_, systemSymbol_) {
+        const localData = require("../user-preferences.json");
+
+        let callData = await fetch(sa.address + 'systems/' + systemSymbol_ + '/waypoints/' + waypointSymbol_ + '/shipyard', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localData.token
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                return {
+                    error: {
+                        title: "NavigationAPICalls.getShipyard Error",
+                        message: error
+                    }
+                }
+            })
+
+        return callData;
+    },
+
+
+    /** 
+     * Get jump gate details for a waypoint. Requires a waypoint of type [JUMP_GATE] to use.
+     * The response will return all systems that have a jump gate in range of this jump gate. Those systems can be jumped to from this location.
+     * https://spacetraders.stoplight.io/docs/spacetraders/decd101af6414-get-jump-gate
+     * @param {string} waypointSymbol_ The name of the waypoint with the [JUMP_GATE] trait.
+     * @param {string} systemSymbol_ The name of the system that the jump gate waypoint is in.
+     */
+    getJumpGate: async function (waypointSymbol_, systemSymbol_) {
+        const localData = require("../user-preferences.json");
+
+        let callData = await fetch(sa.address + 'systems/' + systemSymbol_ + '/waypoints/' + waypointSymbol_ + '/jump-gate', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localData.token
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                return {
+                    error: {
+                        title: "NavigationAPICalls.getJumpGate Error",
+                        message: error
+                    }
+                }
+            })
+
+        return callData;
+    },
 }
 
 export default NavigationAPICalls;
--- api-calls/ship-api-calls.js
+++ api-calls/ship-api-calls.js
@@ -4,12 +4,15 @@
 import sa from './server-address';
 const ShipAPICalls = {
     /**
-     * API call to retrieve a list of all ships owned by the logged-in player.
+     * Returns a paginated list of all ships under your agent's ownership.
+     * https://spacetraders.stoplight.io/docs/spacetraders/64435cafd9005-list-ships
+     * @param {int} resultsPerPage_ How many ships will be returned per page. Defaults to 20. Range is 1-20.
+     * @param {int} pageNum_ The page number of the list of ships to view. Defaults to 1.
      */
-    getMyShipList: async function () {
+    listShips: async function (resultsPerPage_=20, pageNum_=1) {
         const localData = require("../user-preferences.json");
 
-        let callData = await fetch(sa.address + 'my/ships', {
+        let callData = await fetch(sa.address + 'my/ships?page=' + pageNum_.toString() + '&limit=' + resultsPerPage_.toString(), {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -23,7 +26,46 @@ const ShipAPICalls = {
             .catch(error => {
                 return {
                     error: {
-                        title: "ShipAPICalls.getMyShipList Error",
+                        title: "ShipAPICalls.listShips Error",
+                        message: error
+                    }
+                }
+            })
+
+        return callData;
+    },
+
+
+    /**
+     * Purchase a ship from a Shipyard. In order to use this function, a ship under your agent's ownership must
+     * be in a waypoint that has the [Shipyard] trait, and the shipyard must sell the type of the desired ship.
+     * https://spacetraders.stoplight.io/docs/spacetraders/403855e2e99ad-purchase-ship
+     * @param {string} shipType_ The type of the ship to purchase. Must be of the following types:
+     * SHIP_PROBE, SHIP_MINING_DRONE, SHIP_INTERCEPTOR, SHIP_LIGHT_HAULER, SHIP_COMMAND_FRIGATE, SHIP_EXPLORER, SHIP_HEAVY_FREIGHTER, SHIP_LIGHT_SHUTTLE, SHIP_ORE_HOUND, SHIP_REFINING_FREIGHTER
+     * @param {string} waypointSymbol_ The name of the waypoint that has the [Shipyard] trait to purchase the ship at.
+     */
+    purchaseShip: async function (shipType_, waypointSymbol_) {
+        const localData = require("../user-preferences.json");
+
+        let callData = await fetch(sa.address + 'my/ships', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localData.token
+            },
+            body: {
+                'shipType': shipType_,
+                'waypointSymbol': waypointSymbol_
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                return {
+                    error: {
+                        title: "ShipAPICalls.purchaseShip Error",
                         message: error
                     }
                 }
